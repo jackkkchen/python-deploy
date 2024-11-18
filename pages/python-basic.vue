13386037a42f4210d3fa2c19d11750223a2699cf
jackkkchen <799904657@qq.com>
2024-11-18 20:46:43 +0800

    generate静态化网站

--- pages/python-basic.vue
+++ pages/python-basic.vue
@@ -4,7 +4,7 @@
     
     <div class="grid gap-4">
       <ContentList path="/python-basic" v-slot="{ list }">
-        <div v-for="article in list" :key="article._path" 
+        <div v-if="list && list.length > 0" v-for="article in list" :key="article._path" 
              class="p-4 border rounded hover:shadow-lg">
           <!-- 修改链接生成方式 -->
           <NuxtLink :to="`/python-basic/${article._file.split('/').pop().replace('.md', '')}`">
@@ -14,6 +14,9 @@
           <!-- 添加调试信息 -->
           <pre class="text-sm mt-2">Debug: {{ article._path }}</pre>
         </div>
+        <div v-else>
+          <p class="text-gray-600">没有找到相关文章。</p>
+        </div>
       </ContentList>
     </div>
   </div>
@@ -21,8 +24,14 @@
 
 <script setup>
 // 添加调试日志
-const { data: articles } = await useAsyncData('articles', () => 
+const { data: articles, error } = await useAsyncData('articles', () => 
   queryContent('/python-basic').find()
 )
+
+// 错误处理
+if (error.value) {
+  console.error('Error fetching articles:', error.value)
+  // 可以在这里显示一个用户友好的错误消息
+}
 console.log('Available articles:', articles.value)
 </script>
